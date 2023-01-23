2804abff9e90cd3275660ed975e4ccd8a22303a4
Ramya732807 <98092768+Ramya732807@users.noreply.github.com>
2023-01-23 14:56:05 +0530

    Add files via upload

---
+++ output.py
@@ -0,0 +1,12 @@
+Please enter the number of jobs: 3
+Please enter start time: 0900
+Please enter end time: 1000
+Please enter profit value: 250
+Please enter start time: 0945
+Please enter end time: 1200
+Please enter profit value: 550
+Please enter start time: 1130
+Please enter end time: 1500
+Please enter profit value: 150
+Number of jobs left for other employees:  2
+Total earnings left for other employees:  400
